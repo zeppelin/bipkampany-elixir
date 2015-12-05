67970367cc0e857827f1d1fe30ea3c81e1d41efb
Gabor Babicz <gabor.babicz@gmail.com>
2015-12-05 23:49:04 +0100

    Initial implementation WIP

--- lib/bip_kampany.ex
+++ lib/bip_kampany.ex
@@ -1,2 +1,77 @@
-defmodule BipKampany do
+defmodule BipKampany.Config do
+  defstruct [:email, :password, params: %{}]
+
+  def new([email: email, password: password]) do
+    %BipKampany.Config{
+      email: email,
+      password: password
+    }
+  end
+end
+
+defmodule BipKampany.Api do
+  alias BipKampany.Config
+
+  @url "http://api.bipkampany.hu/"
+
+  def get_balance(%Config{} = config) do
+    config |> call("getbalance")
+  end
+
+  def get_charset(%Config{} = config) do
+    config |> call("getcharset")
+  end
+
+  def send_sms(%Config{} = config, phone_number, message, sender_id) do
+    params = %{
+      number: phone_number,
+      message: message,
+      senderid: sender_id
+    }
+
+    config
+    |> assign_params(params)
+    |> call("sendsms")
+  end
+
+  def cancel_sms(%Config{} = config, reference_ids \\ []) do
+    config
+    |> assign_params(%{referenceid: Enum.join(reference_ids, ",")})
+    |> call("cancelsms")
+  end
+
+
+  defp call(config, action) do
+    base_url = "#{@url}#{action}"
+
+    # Somehow `URI.encode_query` encodes stuff like `@` and `,`
+    query_params =
+      prepare_params(config)
+      |> URI.encode_query
+      |> URI.decode
+
+    url = "#{base_url}?#{query_params}"
+    {_status, response} = HTTPoison.get(url)
+
+    Poison.decode! response.body
+  end
+
+  defp prepare_params(%Config{} = config) do
+    credentials = %{
+      email: config.email,
+      password: config.password
+    }
+
+    defaults = %{
+      format: "json"
+    }
+
+    config.params
+    |> Dict.merge(credentials)
+    |> Dict.merge(defaults)
+  end
+
+  defp assign_params(%Config{} = config, %{} = params) do
+    Map.put(config, :params, params)
+  end
 end
--- test/bip_kampany_test.exs
+++ test/bip_kampany_test.exs
@@ -1,7 +1,90 @@
 defmodule BipKampanyTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+  import Mock
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  defp httpoison_response_json(dict) do
+    { :ok, %{ body: Poison.encode!(dict) } }
+  end
+
+
+  setup do
+    {:ok, config: %BipKampany.Config{
+        email: "gabor.babicz@gmail.com",
+        password: "hello",
+        params: %{}
+      }
+    }
+  end
+
+
+  test "config object", context do
+    config = BipKampany.Config.new([
+      email: "gabor.babicz@gmail.com",
+      password: "hello"
+    ])
+
+    assert config == context.config
+  end
+
+  test "get balance", context do
+    response_body = httpoison_response_json(%{
+        result: "OK",
+        message: "A kerest sikeresen teljesitettuk.",
+        balance: "30.00",
+        currency: "HUF",
+        limit: "40000"
+    })
+
+    with_mock HTTPoison, [get: fn(_url) -> response_body end] do
+      result = context.config |> BipKampany.Api.get_balance
+
+      assert called HTTPoison.get("http://api.bipkampany.hu/getbalance?email=gabor.babicz@gmail.com&format=json&password=hello")
+      assert result == %{
+        "result" => "OK",
+        "message" => "A kerest sikeresen teljesitettuk.",
+        "balance" => "30.00",
+        "currency" => "HUF",
+        "limit" => "40000"
+      }
+    end
+  end
+
+  test "get charset", context do
+    response_body = httpoison_response_json(%{
+      result: "OK",
+      message: "A kerest sikeresen teljesitettuk.",
+      charset: "@\u00a3$\u00a5\u00e8\u00e9\u00f9\u00ec\u00f2\u00c7\u00d8\u00f8\u00c5\u00e5\u0394_\u03a6\u0393\u039b\u03a9\u03a0\u03a8\u03a3\u0398\u039e\u00c6\u00e6\u00df\u00c9 !\"#\u00a4%&'()*+,-.\/0123456789:;<=>?\u00a1ABCDEFGHIJKLMNOPQRSTUVWXYZ\u00c4\u00d6\u00d1\u00dc\u00a7\u00bfabcdefghijklmnopqrstuvwxyz\u00e4\u00f6\u00f1\u00fc\u00e0\u00e1\u00ed\u00e9\u00f3\u00fa\u00c1\u00cd\u00da\u00d3\u0151\u0150\u0171\u0170"
+    })
+
+    with_mock HTTPoison, [get: fn(_url) -> response_body end] do
+      result = context.config |> BipKampany.Api.get_charset
+
+      assert called HTTPoison.get("http://api.bipkampany.hu/getcharset?email=gabor.babicz@gmail.com&format=json&password=hello")
+      assert result == %{
+        "result" => "OK",
+        "message" => "A kerest sikeresen teljesitettuk.",
+        "charset" => "@\u00a3$\u00a5\u00e8\u00e9\u00f9\u00ec\u00f2\u00c7\u00d8\u00f8\u00c5\u00e5\u0394_\u03a6\u0393\u039b\u03a9\u03a0\u03a8\u03a3\u0398\u039e\u00c6\u00e6\u00df\u00c9 !\"#\u00a4%&'()*+,-.\/0123456789:;<=>?\u00a1ABCDEFGHIJKLMNOPQRSTUVWXYZ\u00c4\u00d6\u00d1\u00dc\u00a7\u00bfabcdefghijklmnopqrstuvwxyz\u00e4\u00f6\u00f1\u00fc\u00e0\u00e1\u00ed\u00e9\u00f3\u00fa\u00c1\u00cd\u00da\u00d3\u0151\u0150\u0171\u0170"
+      }
+    end
+  end
+
+  test "cancel sms", context do
+    response_body = httpoison_response_json(%{
+      result: "OK",
+      code: 0,
+      message: "A kerest sikeresen teljesitettuk.",
+    })
+
+    with_mock HTTPoison, [get: fn(_url) -> response_body end] do
+      result = context.config
+      |> BipKampany.Api.cancel_sms([1, 2, 3])
+
+      assert called HTTPoison.get("http://api.bipkampany.hu/cancelsms?email=gabor.babicz@gmail.com&format=json&password=hello&referenceid=1,2,3")
+      assert result == %{
+        "result" => "OK",
+        "code" => 0,
+        "message" => "A kerest sikeresen teljesitettuk."
+      }
+    end
   end
 end
